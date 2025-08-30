94b670b22db1b6e0982837b9502812fb5063e9c4
MohaMed Nabil <mohamednabilpro2024@gmail.com>
2025-08-30 18:07:08 +0300

    staticfiles upload

--- project/settings.py
+++ project/settings.py
@@ -138,6 +138,16 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/usr/src/app/static/",
+# ]
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+
 # =========================
 # DEFAULT SETTINGS
 # =========================
